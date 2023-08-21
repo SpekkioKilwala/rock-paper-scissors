ea004478073fa2c11bcc8ff9c34a42e5ee89aac8
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-21 20:22:55 +1000

    Add ability to compare two moves to find the winner

--- index.js
+++ index.js
@@ -4,14 +4,47 @@
 
 const moves = ["rock", "paper", "scissors"]
 
-
 function randPick(choices) {
     // given an array of items, pick one of them
     const pick = Math.floor(Math.random() * choices.length);
     return choices[pick];
 }
 
-function getComputerChoice() {
+function getComputerMove() {
     // randomly pick "rock", "paper", or "scissors"
     return randPick(moves);
+}
+
+function matchUp(playerMove, computerMove) {
+    // I don't like hard-coding who's who but whatever
+    playerMove = playerMove.toLowerCase()
+    computerMove = computerMove.toLowerCase()
+    if (playerMove == computerMove) {
+        return "Draw!"
+    }
+    switch (playerMove) {
+        case "rock":
+            // The victory/loss messages can be refactored/unified completely.
+            if (computerMove == "scissors") {
+                return "You win! Rock beats scissors."
+            } else {
+                return "You lose! Paper beats rock."
+            }
+        case "paper":
+            if (computerMove == "rock") {
+                return "You win! Paper beats rock."
+            } else {
+                return "You lose! Scissors beat paper."
+            }
+        case "scissors":
+            if (computerMove == "paper") {
+                return "You win! Scissors beat paper."
+            } else {
+                return "You lose! Rock beats scissors."
+            }
+        default:
+            return "invalid"
+    }
+
+
 }
