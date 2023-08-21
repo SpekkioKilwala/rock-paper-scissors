d535134bd154891e8a74a1713191ad31ab642773
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-21 20:57:37 +1000

    Add ability to play a round and record the result

--- index.js
+++ index.js
@@ -3,6 +3,18 @@
 // alert("JS is running");
 
 const moves = ["rock", "paper", "scissors"]
+let wins = 0;
+let draws = 0;
+let losses = 0;
+
+function newGame() {
+    // Resets all the tally stuff to zero
+    wins = draws = losses = 0;
+}
+
+function roundsPlayed() {
+    return (wins + draws + losses);
+}
 
 function randPick(choices) {
     // given an array of items, pick one of them
@@ -20,31 +32,54 @@ function matchUp(playerMove, computerMove) {
     playerMove = playerMove.toLowerCase()
     computerMove = computerMove.toLowerCase()
     if (playerMove == computerMove) {
-        return `Draw! Both chose ${playerMove}.`
+        return `draw`
     }
     switch (playerMove) {
         case "rock":
             // The victory/loss messages can be refactored/unified completely.
             if (computerMove == "scissors") {
-                return "You win! Rock beats scissors."
+                return "win"
             } else {
-                return "You lose! Paper beats rock."
+                return "lose"
             }
         case "paper":
             if (computerMove == "rock") {
-                return "You win! Paper beats rock."
+                return "win"
             } else {
-                return "You lose! Scissors beat paper."
+                return "lose"
             }
         case "scissors":
             if (computerMove == "paper") {
-                return "You win! Scissors beat paper."
+                return "win"
             } else {
-                return "You lose! Rock beats scissors."
+                return "lose"
             }
         default:
             return "invalid"
     }
+}
 
-
+function playRound() {
+    // Accept player and computer moves, see who the winner is, add the result to the totals.
+    let playerMove = prompt("Pick: rock, paper, or scissors!");
+    let computerMove = getComputerMove();
+    let roundOutcome = matchUp(playerMove, computerMove);
+    console.log(roundOutcome);
+    switch (roundOutcome) {
+        case "win":
+            console.log(`You win! ${playerMove} beats ${computerMove}.`);
+            wins++;
+            break;
+        case "draw":
+            console.log(`Draw! Both picked ${playerMove}.`);
+            draws++;
+            break;
+        case "lose":
+            console.log(`You lose! ${playerMove} loses to ${computerMove}.`);
+            losses++;
+            break;
+        default:
+            console.log("Invalid response, discarding round result.");
+    }
+    console.log(`Wins: ${wins} | Draws: ${draws} | Losses: ${losses}`);
 }
