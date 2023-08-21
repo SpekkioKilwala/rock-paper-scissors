dbed6ea2c17106b82bec4cf48806674a972a9746
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2023-08-21 19:49:07 +1000

    Add functions for picking a random computer move

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 "use strict";
 
-alert("JS is running");
+// alert("JS is running");
+
+const moves = ["rock", "paper", "scissors"]
+
+
+function randPick(choices) {
+    // given an array of items, pick one of them
+    const pick = Math.floor(Math.random() * choices.length);
+    return choices[pick];
+}
+
+function getComputerChoice() {
+    // randomly pick "rock", "paper", or "scissors"
+    return randPick(moves);
+}
